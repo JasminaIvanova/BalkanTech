d68fd11425278d9170666d27073d4706e3cfa4ba
JasminaIvanova <jasmina.j.ivanova@gmail.com>
2024-11-05 00:54:58 +0200

    adjustments for task categories / added in the view as filtering

--- BalkanTech/BalkanTech.Web.ViewModels/obj/Debug/net8.0/BalkanTech.Web.ViewModels.AssemblyInfo.cs
+++ BalkanTech/BalkanTech.Web.ViewModels/obj/Debug/net8.0/BalkanTech.Web.ViewModels.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BalkanTech.Web.ViewModels")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f62bcf7155480fdd2bd8cea085555e9057359fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90d93c0688182e26271a9f869520530b7ff31fe7")]
 [assembly: System.Reflection.AssemblyProductAttribute("BalkanTech.Web.ViewModels")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BalkanTech.Web.ViewModels")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- BalkanTech/BalkanTech/Controllers/TaskController.cs
+++ BalkanTech/BalkanTech/Controllers/TaskController.cs
@@ -1,14 +1,24 @@
-﻿using BalkanTech.Web.ViewModels;
+﻿using BalkanTech.Data;
+using BalkanTech.Services.Data.Interfaces;
+using BalkanTech.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BalkanTech.Web.Controllers
 {
     public class TaskController : Controller
     {
+        private readonly BalkanDbContext context;
+        public TaskController(BalkanDbContext _context)
+        {
+            context = _context;
+        }
+        [HttpGet]
         public IActionResult Index(Guid id, int roomNumber)
         {
+            //TODO -> service
             var model = new TaskViewModel();
             model.RoomNumber = roomNumber; 
+            model.Categories = context.TaskCategories.Select(c => c.Name).ToList();
             return View(model);
         }
     }
